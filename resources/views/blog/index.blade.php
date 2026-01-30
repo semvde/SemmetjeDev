bc8ff6fde7deacb470acccbbca3242c158461c6c
Semmetje <semvandenende@gmail.com>
2026-01-30 14:58:56 +0100

    Updated Vite configuration for deployment

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,7 +3,7 @@
         <header
             class="flex flex-col justify-center items-center bg-gradient-to-bl from-sky-900 via-transparent to-transparent h-40">
             <span>✈️ My learning journey!</span>
-            <h1 class="text-white">Blog</h1>
+            <h1 class="text-white">Blog Posts</h1>
         </header>
     </x-slot>
 
@@ -15,7 +15,7 @@
         <div class="grid grid-cols-1 gap-5">
             @forelse($posts as $post)
                 <a href="{{ route('blog.show', $post) }}">
-                    
+
                 </a>
             @empty
                 <p class="text-center">Looks like there aren't any public blog posts currently! Come back soon!</p>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js',
+                'resources/js/admin-publishing.js', 'resources/js/admin-projectimages.js', 'resources/js/admin-projecttechnologies.js'],
             refresh: true,
         }),
     ],
